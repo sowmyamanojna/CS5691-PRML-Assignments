7a80d3ba14de38f5c1d43eb6422bed0665f25e7f
sowmyamanojna <sowmyamanojna@gmail.com>
2021-04-22 03:14:50 +0530

    Added the utils helper script

---
+++ assignment2/codes/utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+def get_consolidated_data(classes_present):
+    df = pd.DataFrame()
+    df_test = pd.DataFrame()
+    for i in classes_present:
+        df_new = pd.read_csv("../datasets/2A/"+i+"/train.csv")
+        df_new["image_names"] = classes_present[i]
+        df_new = df_new.rename(columns={"image_names":"class"})
+        df = df.append(df_new)
+
+        df_new_test = pd.read_csv("../datasets/2A/"+i+"/dev.csv")
+        df_new_test["image_names"] = classes_present[i]
+        df_new_test = df_new_test.rename(columns={"image_names":"class"})
+        df_test = df_test.append(df_new_test)
+
+    df.to_csv("../datasets/2A/consolidated_train.csv", index=False)
+    df_test.to_csv("../datasets/2A/consolidated_dev.csv", index=False)
+
+if __name__ == "__main__":
+    classes_present = {"coast":0, "highway":1, "mountain":2, "opencountry":3, "tallbuilding":4}
+    get_consolidated_data(classes_present)
