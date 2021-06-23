b1fff2372e1c77ebf2126f45ea37ffa873aa5147
sowmyamanojna <sowmyamanojna@gmail.com>
2021-06-23 12:19:02 +0530

    Minor changes

--- assignment2/codes/gmm.py
+++ assignment2/codes/gmm.py
@@ -250,6 +250,109 @@ class GMM_2A():
         self.val = val
         return np.sum(val, axis=1)
 
+class GMM_new():
+    def __init__(self, q, tol=1e-3):
+        self.q = q
+        self.tol = tol
+
+    def fit(self, X, covariance_type="full", epochs=100, tol=1e-6):
+        """
+        X: n*d
+        mu: q*d
+        C: q*d*d
+        gamma: n*q
+        """
+        self.n, self.d = X.shape    
+        self.X = X
+        self.epochs = epochs
+        self.covariance_type = covariance_type
+        self.tol = tol
+        
+        self.initialization()
+
+        self.lglk_list = []
+
+        for _ in range(self.epochs):
+            self.lglk_list.append(self.log_likelihood(self.X))
+            self.expectation()
+            self.maximization()
+            # new_lk = self.log_likelihood(self.X)
+            # diff = new_lk - self.lglk_list[-1]
+            # if  diff < 0:
+                # if diff < 0: print("Difference is less than 0")
+                # break
+
+    def initialization(self):
+        self.C = 5*np.zeros((self.q, self.d, self.d))
+        for i in range(self.q):
+            np.fill_diagonal(self.C[i], 5)
+
+        self.mu = np.random.randint(-1, 1, size=(self.q, self.d))
+        self.weights = np.ones((self.q, 1))/self.q
+        self.gamma = np.zeros((self.n, self.q))
+        self.reg_C = self.tol*np.eye(self.d)
+
+    def expectation(self):
+        self.gamma = np.zeros((self.n, self.q))
+        # Update the gamma
+        for i in range(self.q):
+            self.gamma[:,i] = self.weights[i]*mvn.pdf(self.X, self.mu[i], self.C[i]+self.reg_C)
+        # Normalize the gamma
+        self.gamma = self.gamma/np.sum(self.gamma, axis=1).reshape(-1,1)
+
+    def maximization(self):
+        self.Nq = np.sum(self.gamma, axis=0)
+        self.mu = (self.gamma.T @ self.X)/self.Nq.reshape(-1,1)
+        
+        for i in range(self.q):
+            self.C[i] = (1/self.Nq[i])*(self.gamma[:,i].reshape(-1,1)*(self.X-self.mu[i,:])).T@(self.X-self.mu[i,:])
+            self.C[i] += self.reg_C
+
+            if self.covariance_type == "diag":
+                self.C[i] = np.diag(self.C[i])
+
+        self.weights = self.Nq/self.n
+
+    def log_likelihood(self, X_test):
+        lk = 0
+        n, d = X_test.shape
+        for i in range(n):
+            val = 0
+            for j in range(self.q):
+                val += self.weights[j]*mvn.pdf(X_test[i], self.mu[j], self.C[j])
+            lk += np.log(val)
+        return lk
+
+    def indv_log_likelihood(self, X_test):
+        n, d = X_test.shape
+        lk = np.zeros((X_test.shape[0], 1))
+        for i in range(n):
+            val = 0
+            for j in range(self.q):
+                try:
+                    val += self.weights[j]*mvn.pdf(X_test[i], self.mu[j], self.C[j])
+                except:
+                    val += self.weights[j]*mvn.pdf(X_test[i], self.mu[j], self.C[j]+np.eye(self.C[j].shape[0])*1e-7)
+            lk[i] = np.log(val)
+
+        return lk
+
+    def mvg(self, X, m, C):
+        n, d = X.shape
+        denom = (1/(((2*np.pi)**(d/2))*np.sqrt(np.linalg.det(C))))
+        exp = (-(X-m).T@np.linalg.pinv(C)@(X-m))/2
+        return denom*np.exp(exp)
+
+    def gaussian_val(self, X_test):
+        n, d = X_test.shape
+        val = np.zeros((n, self.q))
+
+        for i in range(self.q):
+            val[:,i] = self.weights[i]*mvn.pdf(X_test, self.mu[i], self.C[i])
+
+        self.val = val
+        return np.sum(val, axis=1)
+
 class GMM_2B():
     def __init__(self, q, tol=1e-3):
         self.q = q
